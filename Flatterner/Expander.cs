2b58cfa29e32ec6b2c7ae5366581b72f62f2402a
ambro-r <rrambrosio@gmail.com>
2021-06-16 17:26:24 +0200

    Added support for expanding basic string.

--- Flatterner/Expander.cs
+++ Flatterner/Expander.cs
@@ -24,11 +24,29 @@ namespace Formatter
                 IOrderedEnumerable<PropertyInfo> properties = FormatHelper.GetOrderedFormats(type);
                 foreach (PropertyInfo property in properties)
                 {
-                    Format flat = FormatHelper.GetFormat(property);
+                    Format format = FormatHelper.GetFormat(property);
+                    string piece = GetPiece(format, flatString, offsetAdjustment);
+                    property.SetValue(expand, Convert.ChangeType(piece, property.PropertyType), null);
                 }
 
             }
             return expand;
         }
+
+        private string GetPiece(Format format, string flatString, int offsetAdjustment)
+        {
+            string piece = flatString.Substring(format.Offset - offsetAdjustment, format.Length);
+            char fill = format.Fill.Length > 0 ? format.Fill[0] : ' ';
+            if (format.Justified == Constants.Justified.LEFT)
+            {
+                piece = piece.TrimEnd(fill);
+            }
+            else if (format.Justified == Constants.Justified.RIGHT)
+            {
+                piece = piece.TrimStart(fill);
+            }
+            return piece.Trim();
+        }
+
     }
 }
